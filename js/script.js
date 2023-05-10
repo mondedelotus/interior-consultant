49c6a669178569ebf8fcf7647246e663b05fa7e7
mondedelotus <mondedelotus@gmail.com>
2023-05-11 00:23:34 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Menu
+
+const burger = document.querySelector('.menu__icon');
+const menu = document.querySelector('.menu');
+const body = document.body
+
+if (burger && menu) {
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('_active');
+		menu.classList.toggle('_active');
+		body.classList.toggle('_lock');
+	})
+}
+
+const menuItems = document.querySelectorAll('.menu__link');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', () => {
+    menuItems.forEach(item => item.classList.remove('active'));
+    item.classList.add('active');
+  });
+});
